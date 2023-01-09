607bf72a35eb65df2005179315c020d5595b568c
Ashwin Gur <ashwingur88@gmail.com>
2023-01-10 10:23:17 +1100

    Improving project card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -3,28 +3,42 @@ import React from "react";
 import Image from "next/image";
 
 interface ProjectProps {
+  id: number;
   title: string;
   image: string;
   description: string;
+  github: string;
+  stack: Array<String>;
 }
 
-const ProjectCard = ({ title, image, description }: ProjectProps) => {
+const ProjectCard = ({
+  id,
+  title,
+  image,
+  description,
+  github,
+  stack,
+}: ProjectProps) => {
+  var stack_items = stack.map((item) => <span key={id}>{item}</span>);
+
   return (
     <div className="flex flex-col justify-center rounded-2xl bg-white max-w-lg shadow-xl relative m-8 hover:bg-gray-100 hover:shadow-2xl cursor-pointer">
-      {/* <Image
-        src={image}
-        alt="thumbnail"
-        // width="0"
-        // height="0"
-        className="w-full"
-        style={{ width: "100%" }}
-      /> */}
-      <img className="rounded-t-lg" src={image} alt="" />
+      <a href={github} target="_blank" rel="noreferrer">
+        <div className="w-full h-72 relative">
+          <Image
+            alt="Mountains"
+            src={image}
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
 
-      <div className="p-8">
-        <h2 className="mb-2 text-2xl">{title}</h2>
-        <p>{description}</p>
-      </div>
+        <div className="p-8">
+          <h2 className="mb-2 text-2xl">{title}</h2>
+          <p>{description}</p>
+        </div>
+        {stack_items}
+      </a>
     </div>
   );
 };
