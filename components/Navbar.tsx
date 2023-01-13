a415860f3d7d0479a38a3042becb4bdbf8537434
Ashwin Gur <ashwingur88@gmail.com>
2023-01-13 20:50:43 +1100

    Adding nav menu for mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { MdLightMode, MdDarkMode } from "react-icons/md";
+import {
+  MdLightMode,
+  MdDarkMode,
+  MdOutlineMenu,
+  MdOutlineClose,
+} from "react-icons/md";
 
 interface NavProps {
   toggleTheme(): any;
@@ -10,53 +15,133 @@ interface NavProps {
 
 const Navbar = ({ toggleTheme, initialTheme }: NavProps) => {
   let [currentTheme, setTheme] = useState(initialTheme);
+  let [mobileNavMenu, setMobileNavMenu] = useState(false); // Mobile nav menu not showing at the start
 
   return (
-    <div className="flex fixed w-full justify-between px-16 py-4 shadow-lg backdrop-blur-md bg-white/30 dark:bg-white/10 z-50">
-      <Link href="/">
-        <Image src="/logo.png" alt="logo" width="80" height="80" />
-      </Link>
-
-      <ul className="flex gap-4 items-center">
-        <Link
-          href="/#home"
-          className="hover:bg-blue-100 dark:hover:bg-black px-2 py-1 transition rounded-md"
-          scroll={false}
-        >
-          <li>Home</li>
+    <div>
+      <div className="flex fixed w-full justify-between px-4 md:px-8 lg:px-16 py-4 shadow-lg backdrop-blur-md bg-white/30 dark:bg-white/10 z-50">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width="80" height="80" />
         </Link>
-        <Link
-          href="/#projects"
-          className="hover:bg-blue-100 dark:hover:bg-black px-2 py-1 transition rounded-md"
-          scroll={false}
-        >
-          <li>Projects</li>
-        </Link>
-        <Link
-          href="/#other"
-          className="hover:bg-blue-100 dark:hover:bg-black px-2 py-1 transition rounded-md"
-          scroll={false}
-        >
-          <li>Other</li>
-        </Link>
-        <Link
-          href="/#contact"
-          className="hover:bg-blue-100  dark:hover:bg-black px-2 py-1 transition rounded-md"
-          scroll={false}
-        >
-          <li>Contact</li>
-        </Link>
-        <li>
-          <button
-            className=" hover:bg-blue-100 dark:hover:bg-black p-2 rounded-md"
-            onClick={() => {
-              setTheme(toggleTheme());
-            }}
+        <ul className="hidden lg:flex lg:gap-4 lg:items-center) ">
+          <Link
+            href="/#home"
+            className="hover:bg-blue-100 dark:hover:bg-black px-2 py-1 transition rounded-md"
+            scroll={false}
           >
-            {currentTheme === "light" ? <MdDarkMode /> : <MdLightMode />}
-          </button>
-        </li>
-      </ul>
+            <li>Home</li>
+          </Link>
+          <Link
+            href="/#projects"
+            className="hover:bg-blue-100 dark:hover:bg-black px-2 py-1 transition rounded-md"
+            scroll={false}
+          >
+            <li>Projects</li>
+          </Link>
+          <Link
+            href="/#other"
+            className="hover:bg-blue-100 dark:hover:bg-black px-2 py-1 transition rounded-md"
+            scroll={false}
+          >
+            <li>Other</li>
+          </Link>
+          <Link
+            href="/#contact"
+            className="hover:bg-blue-100  dark:hover:bg-black px-2 py-1 transition rounded-md"
+            scroll={false}
+          >
+            <li>Contact</li>
+          </Link>
+          <li>
+            <button
+              className=" hover:bg-blue-100 dark:hover:bg-black p-2 rounded-md"
+              onClick={() => {
+                setTheme(toggleTheme());
+              }}
+            >
+              {currentTheme === "light" ? <MdDarkMode /> : <MdLightMode />}
+            </button>
+          </li>
+        </ul>
+        <div
+          className="cursor-pointer lg:hidden"
+          onClick={() => {
+            setMobileNavMenu(true);
+          }}
+        >
+          <MdOutlineMenu size={25} />
+        </div>
+      </div>
+      {/* MOBILE NAV MENU */}
+      <div
+        className={
+          mobileNavMenu
+            ? "fixed w-full h-screen bg-black/70 top-0 left-0 z-50"
+            : "z-50"
+        }
+        onClick={() => setMobileNavMenu(false)}
+      >
+        <div
+          className={
+            mobileNavMenu
+              ? "absolute left-0 top-0 w-[75%] h-full bg-slate-50 dark:bg-slate-800 ease-in duration-500 z-50"
+              : "fixed left-[-100%] top-0 h-full p-10 ease-in duration-500 z-50"
+          }
+        >
+          <div className="flex justify-between items-center mx-8 py-4 border-b">
+            <Link href="/">
+              <Image src="/logo.png" alt="logo" width="80" height="80" />
+            </Link>
+            <MdOutlineClose
+              size={40}
+              className="hover:bg-blue-100 transition rounded-full p-2 cursor-pointer"
+              onClick={() => {
+                setMobileNavMenu(false);
+              }}
+            />
+          </div>
+          <ul className="flex flex-col mx-8 mt-4">
+            <Link
+              href="/#home"
+              className="hover:bg-blue-100 dark:hover:bg-black px-2 py-1 transition rounded-md my-2"
+              scroll={false}
+            >
+              <li>Home</li>
+            </Link>
+            <Link
+              href="/#projects"
+              className="hover:bg-blue-100 dark:hover:bg-black px-2 py-1 transition rounded-md my-2"
+              scroll={false}
+            >
+              <li>Projects</li>
+            </Link>
+            <Link
+              href="/#other"
+              className="hover:bg-blue-100 dark:hover:bg-black px-2 py-1 transition rounded-md my-2"
+              scroll={false}
+            >
+              <li>Other</li>
+            </Link>
+            <Link
+              href="/#contact"
+              className="hover:bg-blue-100  dark:hover:bg-black px-2 py-1 transition rounded-md my-2"
+              scroll={false}
+            >
+              <li>Contact</li>
+            </Link>
+            <li>
+              <button
+                className=" hover:bg-blue-100 dark:hover:bg-black p-2 rounded-md my-2"
+                onClick={() => {
+                  setTheme(toggleTheme());
+                }}
+              >
+                {currentTheme === "light" ? <MdDarkMode /> : <MdLightMode />}
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
