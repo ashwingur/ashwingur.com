696d527f5ed40101d49adcfdd89b1a127efbccf7
Ashwin Gur <ashwingur88@gmail.com>
2023-02-05 00:13:12 +1100

    API proxy calls working for COC now

--- pages/ClashOfClans.tsx
+++ pages/ClashOfClans.tsx
@@ -1,29 +1,38 @@
 import axios from "axios";
-import React, { useEffect } from "react";
-import { Player } from "clashofclans.js";
+import React, { useEffect, useState } from "react";
+import BasicNavbar from "../components/BasicNavbar";
+// import { Player } from "clashofclans.js";
 
 // My profile: #YLPGLJOV
 
-const ClashOfClans = () => {
-  useEffect(() => {
-    // axios.get("/api/clashofclans/player").then((response) => {
-    //   const playerData: Player = response.data;
-    //   console.log(playerData.name);
-    //   console.log(playerData);
-    // });
+// Model from the documentation
+interface Player {
+  role: string;
+  warPreference: string;
+  attackWins: number;
+  defenseWins: number;
+  townHallLevel: number;
+  townHallWeaponLevel: number;
+  versusBattleWins: number;
+  name: string;
+}
 
+const ClashOfClans = () => {
+  const [playerData, setPlayerData] = useState<Player>();
+  useEffect(() => {
     axios
-      .get("https://cocproxy.royaleapi.dev/v1/players/%23YLPGLJOV", {
-        headers: {
-          Authorization:
-            "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjU1YTI0MmJkLTk0OGMtNDVlMC1iOGFiLWE2ZTAxNzJiNDkxMyIsImlhdCI6MTY3NTUxMzE3Mywic3ViIjoiZGV2ZWxvcGVyL2U2MTQyMGVkLTU4ZTgtMWE0Yy0zN2Y3LWJmM2ZkYWNmNDYxNCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjQ1Ljc5LjIxOC43OSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.Mc_2fdq5yCdHpVFHSME6SQ9iTp5AL3MqB-W6MEMHefjEy6WWrprvyHJUCDFK0EdzF-TivT5CPtCVmBwxbv4bVQ",
-        },
-      })
+      .get("/api/clashofclans/player")
       .then((response) => {
-        console.log(response.data);
-      });
+        setPlayerData(response.data);
+      })
+      .catch((error) => console.log("Player fetch error: " + error));
   }, []);
-  return <div>ClashOfClans</div>;
+  return (
+    <div>
+      <BasicNavbar absolute={false} />
+      {JSON.stringify(playerData?.name)}
+    </div>
+  );
 };
 
 export default ClashOfClans;
--- pages/api/clashofclans/player.ts
+++ pages/api/clashofclans/player.ts
@@ -1,16 +1,26 @@
-import { Client, Player } from "clashofclans.js";
+import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const client = new Client();
-  await client.login({
-    email: process.env.COC_DEV_EMAIL!,
-    password: process.env.COC_DEV_PASS!,
+  // https://stackoverflow.com/questions/65058598/nextjs-cors-issue
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
-  const player: Player = await client.getPlayer("#YLPGLJOV");
-  console.log(player.name);
-  res.status(200).json(player);
+
+  axios
+    .get("https://cocproxy.royaleapi.dev/v1/players/%23YLPGLJOV", {
+      headers: {
+        Authorization: `Bearer ${process.env.COC_BEARER_TOKEN}`,
+      },
+    })
+    .then((response) => {
+      res.status(200).json(response.data);
+    });
 }
