7620600b341230c6154f7f7e4214891403502f78
Ashwin Gur <ashwingur88@gmail.com>
2023-02-06 22:48:02 +1100

    Clash font working

--- components/CocPlayerSummary.tsx
+++ components/CocPlayerSummary.tsx
@@ -8,7 +8,7 @@ interface CocPlayerSummaryProps {
 const CocPlayerSummary = ({ player }: CocPlayerSummaryProps) => {
   return (
     <div>
-      <div className="flex items-center gap-4 font-clash font-bold text-white [text-shadow:_0_2px_2px_black,_0_2px_2px_black,_0_2px_2px_black,_0_2px_2px_black,_0_2px_2px_black]">
+      <div className="flex items-center gap-4 font-clash text-white [text-shadow:_0_2px_2px_black,_0_2px_2px_black,_0_2px_2px_black,_0_2px_2px_black,_0_2px_2px_black]">
         {player.expLevel}
         <div>
           <div>{player.name}</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        clash: ["Clash-Regular"],
+        clash: ["Clash-Regular", "sans-serif"],
       },
     },
   },
