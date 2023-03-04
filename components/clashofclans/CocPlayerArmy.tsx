aee69c2d6c22dea805cc2d31b5515498159759f3
Ashwin Gur <ashwingur88@gmail.com>
2023-03-04 22:25:13 +1100

    Troop categories working

--- components/clashofclans/CocPlayerArmy.tsx
+++ components/clashofclans/CocPlayerArmy.tsx
@@ -9,34 +9,49 @@ interface CocPlayerArmyProps {
 interface ArmyItemsCategoryProps {
   items: PlayerItemLevel[];
   category: string;
+  showLevel: boolean;
 }
 
-const ArmyItemIcon = ({ name, level, maxLevel }: PlayerItemLevel) => {
-  const icon_name: string = name.replaceAll(" ", "_");
-  console.log(`og: ${name}, icon: ${icon_name}`);
+interface ArmyItemIconProps {
+  playerItemLevel: PlayerItemLevel;
+  showLevel: boolean;
+}
+
+const ArmyItemIcon = ({ playerItemLevel, showLevel }: ArmyItemIconProps) => {
+  const icon_name: string = playerItemLevel.name.replaceAll(" ", "_");
   return (
     <div className="border-black border-2 inline-block rounded-md relative font-clash font-thin">
       <Image
         src={`/assets/coc/troops/icons/${icon_name}.png`}
-        alt={name}
+        alt={playerItemLevel.name}
         width={64}
         height={64}
       />
-      <div
-        className={
-          "inline-block absolute bottom-1 left-1 rounded-md w-6 h-6 shadow-[0_0px_3px_1px_rgba(0,0,0,0.3)] shadow-white" +
-          (level != maxLevel ? " bg-black" : " bg-yellow-500")
-        }
-      >
-        <div className="text-white px-[2px] text-center">{level}</div>
-      </div>
+      {showLevel && (
+        <div
+          className={
+            "inline-block absolute bottom-1 left-1 rounded-md w-6 h-6 shadow-[0_0px_3px_1px_rgba(0,0,0,0.3)] shadow-white" +
+            (playerItemLevel.level != playerItemLevel.maxLevel
+              ? " bg-black"
+              : " bg-yellow-500")
+          }
+        >
+          <div className="text-white px-[2px] text-center">
+            {playerItemLevel.level}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
-const ArmyItemsCategory = ({ items, category }: ArmyItemsCategoryProps) => {
+const ArmyItemsCategory = ({
+  items,
+  category,
+  showLevel,
+}: ArmyItemsCategoryProps) => {
   const items_elements = items.map((item, index) => (
-    <ArmyItemIcon key={index} {...item} />
+    <ArmyItemIcon key={index} playerItemLevel={item} showLevel={showLevel} />
   ));
 
   return (
@@ -48,25 +63,102 @@ const ArmyItemsCategory = ({ items, category }: ArmyItemsCategoryProps) => {
 };
 
 const CocPlayerArmy = ({ player }: CocPlayerArmyProps) => {
-  const player_troops = player.troops.map((item, index) => (
-    <ArmyItemIcon key={index} {...item} />
-  ));
+  const siege_machine_names = [
+    "Wall Wrecker",
+    "Battle Blimp",
+    "Stone Slammer",
+    "Siege Barracks",
+    "Log Launcher",
+    "Flame Flinger",
+    "Battle Drill",
+  ];
 
-  const player_spells = player.spells.map((item, index) => (
-    <ArmyItemIcon key={index} {...item} />
-  ));
+  const super_troop_names = [
+    "Super Barbarian",
+    "Super Archer",
+    "Super Giant",
+    "Sneaky Goblin",
+    "Super Wall Breaker",
+    "Rocket Balloon",
+    "Super Wizard",
+    "Super Dragon",
+    "Inferno Dragon",
+    "Super Miner",
+    "Super Minion",
+    "Super Valkyrie",
+    "Super Witch",
+    "Ice Hound",
+    "Super Bowler",
+  ];
+
+  const pet_names = [
+    "L.A.S.S.I",
+    "Electro Owl",
+    "Mighty Yak",
+    "Unicorn",
+    "Frosty",
+    "Diggy",
+    "Poison Lizard",
+    "Phoenix",
+  ];
+
+  const normal_troops = player.troops.filter(
+    (item) =>
+      !(
+        siege_machine_names.includes(item.name) ||
+        super_troop_names.includes(item.name) ||
+        pet_names.includes(item.name) ||
+        item.village == "builderBase"
+      )
+  );
+
+  const siege_machines = player.troops.filter((item) =>
+    siege_machine_names.includes(item.name)
+  );
+
+  const super_troops = player.troops.filter((item) =>
+    super_troop_names.includes(item.name)
+  );
+
+  const pets = player.troops.filter((item) => pet_names.includes(item.name));
+
+  const builder_troops = player.troops.filter(
+    (item) => item.village == "builderBase"
+  );
 
   return (
     <div>
-      <div className="bg-[#5d6b96] p-4 mx-4 md:mx-8 rounded-lg">
-        <div className=" clash-font-style text-xl mb-4">Home Troops</div>
-        <div className="flex flex-row flex-wrap gap-1 ">{player_troops}</div>
-      </div>
-      <div className="bg-[#5d6b96] p-4 mx-4 md:mx-8 rounded-lg my-4">
-        <div className=" clash-font-style text-xl mb-4">Spells</div>
-        <div className="flex flex-row flex-wrap gap-1 ">{player_spells}</div>
-      </div>
-      <ArmyItemsCategory items={player.heroes} category="Heroes" />
+      <ArmyItemsCategory
+        items={normal_troops}
+        category="Troops"
+        showLevel={true}
+      />
+      {/* <ArmyItemsCategory
+        items={super_troops}
+        category="Super Troops"
+        showLevel={false}
+      /> */}
+      <ArmyItemsCategory
+        items={siege_machines}
+        category="Siege Machines"
+        showLevel={true}
+      />
+      <ArmyItemsCategory
+        items={builder_troops}
+        category="Builder Base"
+        showLevel={true}
+      />
+      <ArmyItemsCategory
+        items={player.spells}
+        category="Spells"
+        showLevel={true}
+      />
+      <ArmyItemsCategory
+        items={player.heroes}
+        category="Heroes"
+        showLevel={true}
+      />
+      <ArmyItemsCategory items={pets} category="Pets" showLevel={true} />
     </div>
   );
 };
