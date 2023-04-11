74056494a73103cf8b3d0ea71d128ca2020e03bd
Ashwin Gur <ashwingur88@gmail.com>
2023-04-12 00:05:47 +1000

    added endpoint for clan tracking with mongodb

---
+++ model/CocUser.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema, InferSchemaType } from "mongoose";
+import { Player } from "../shared/interfaces/coc.interface";
+
+interface IData {
+  time: number;
+  player: Player;
+}
+
+interface ICocUser {
+  id: string;
+  data: IData[];
+}
+const userSchema = new Schema<ICocUser>({
+  id: { type: String, required: true },
+  data: [] as IData[],
+});
+
+const CocUser =
+  mongoose.models.CocUser || mongoose.model("CocUser", userSchema);
+export default CocUser;
+// module.exports =
+//   mongoose.models.CocUser || mongoose.model("CocUser", userSchema);
---
+++ pages/api/clashofclans/trackclandata.ts
@@ -0,0 +1,76 @@
+import axios, { AxiosResponse } from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
+import mongoose from "mongoose";
+import CocUser from "../../../model/CocUser";
+// const CocUser = require("../../../model/CocUser");
+import PlayerDataExample from "../../../data/player.json";
+import { Clan, Player } from "../../../shared/interfaces/coc.interface";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // https://stackoverflow.com/questions/65058598/nextjs-cors-issue
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
+  const clanTag = "220QP2GGU";
+  mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@ashwingur.dslbe5g.mongodb.net/clash_of_clans`
+  );
+
+  // Get the clan users
+  try {
+    const clanResponse: AxiosResponse<Clan> = await axios.get(
+      `https://cocproxy.royaleapi.dev/v1/clans/%23${clanTag}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.COC_BEARER_TOKEN}`,
+        },
+      }
+    );
+    const clanData = clanResponse.data;
+
+    // Now go through each member and query them
+    clanData.memberList.forEach(async (player) => {
+      const playerResponse: AxiosResponse<Player> = await axios.get(
+        `https://cocproxy.royaleapi.dev/v1/players/%23${player.tag.replace(
+          "#",
+          ""
+        )}`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.COC_BEARER_TOKEN}`,
+          },
+        }
+      );
+      const playerData = playerResponse.data;
+      if (await CocUser.exists({ id: playerData.tag })) {
+        const user = await CocUser.findOne({ id: playerData.tag });
+        user.data.push({ time: Date.now(), player: playerData });
+        user.save();
+        console.log("saved");
+      } else {
+        console.log("doesnt exist");
+        CocUser.create({
+          id: playerData.tag,
+          data: [{ time: Date.now(), player: playerData }],
+        });
+      }
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({
+        success: "false",
+        error: "something went wrong: " + JSON.stringify(error),
+      });
+  }
+
+  res.status(200).json({ success: "true" });
+}
