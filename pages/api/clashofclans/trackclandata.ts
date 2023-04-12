f797dac09dd5def169b4ab12fe430f331ea5de89
Ashwin Gur <ashwingur88@gmail.com>
2023-04-12 13:21:49 +1000

    Testing clan track fix

--- pages/api/clashofclans/trackclandata.ts
+++ pages/api/clashofclans/trackclandata.ts
@@ -29,6 +29,8 @@ export default async function handler(
     return res.status(500).json({ success: "false", error: error });
   }
 
+  const testData = [];
+
   // Get the clan users
   try {
     const clanResponse: AxiosResponse<Clan> = await axios.get(
@@ -42,7 +44,7 @@ export default async function handler(
     const clanData = clanResponse.data;
 
     // Now go through each member and query them
-    clanData.memberList.forEach(async (player) => {
+    for (const player of clanData.memberList) {
       const playerResponse: AxiosResponse<Player> = await axios.get(
         `https://cocproxy.royaleapi.dev/v1/players/%23${player.tag.replace(
           "#",
@@ -59,12 +61,20 @@ export default async function handler(
         const user = await CocUser.findOne({ id: playerData.tag });
         user.data.push({ time: Date.now(), player: playerData });
         await user.save();
+        testData.push({ tag: playerData.tag, name: playerData.name });
       } else {
         await CocUser.create({
           id: playerData.tag,
           data: [{ time: Date.now(), player: playerData }],
         });
       }
+    }
+
+    console.log("testdata:" + testData);
+    res.status(200).json({
+      success: "true",
+      test: testData,
+      updatedUsers: "is this wokring",
     });
   } catch (error) {
     return res.status(500).json({
@@ -72,6 +82,4 @@ export default async function handler(
       error: "something went wrong: " + JSON.stringify(error),
     });
   }
-
-  res.status(200).json({ success: "true" });
 }
