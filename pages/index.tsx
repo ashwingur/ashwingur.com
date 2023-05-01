8985952ca871f3b209bf4573bb5a17d706cae3fa
Ashwin Gur <ashwingur88@gmail.com>
2023-05-01 20:17:07 +1000

    Adding meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,26 @@ export default function Home() {
     <div>
       <Head>
         <title>Ashwin Gur</title>
-        <meta name="description" content="Ashwin Gur's Portfolio Website" />
+        <meta name="description" content="Ashwin Gur's Website" key="desc" />
+
+        {/* FB Meta Tags */}
+        <meta property="og:title" content="Ashwin Gur" />
+        <meta property="og:description" content="Ashwin Gur's Website" />
+        <meta
+          property="og:image"
+          content="https://www.ashwingur.com/logo.png"
+        />
+        <meta property="og:url" content="https://www.ashwingur.com"></meta>
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:title" content="Ashwin Gur" />
+        <meta name="twitter:description" content="Ashwin Gur's Website" />
+        <meta
+          name="twitter:image"
+          content="https://www.ashwingur.com/logo.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
