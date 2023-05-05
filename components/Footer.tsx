5d11ff495c99d43bdc1a1fa708b9e043af68cdaa
Ashwin Gur <ashwingur88@gmail.com>
2023-05-05 23:31:13 +1000

    Search bar working for media reviews

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import React from "react";
+import { Combobox } from "@headlessui/react";
 
 const Footer = () => {
   return (
-    <div className="bg-slate-900 text-white flex py-2 pl-4 gap-4">
+    <div className="bg-slate-900 text-white flex py-2 ml-4 md:ml-12 gap-4">
       <div>Ashwin Gur</div>
       <Link href={"mailto:agur9842@uni.sydney.edu.au"}>
         E: agur9842@uni.sydney.edu.au
--- pages/MediaReviews.tsx
+++ pages/MediaReviews.tsx
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Reviews from "../data/Reviews.json";
 import Image from "next/image";
-import { AiOutlineArrowDown, AiOutlineArrowUp } from "react-icons/ai";
+import {
+  AiOutlineArrowDown,
+  AiOutlineArrowUp,
+  AiOutlineDown,
+} from "react-icons/ai";
+import { Combobox } from "@headlessui/react";
 
 interface ReviewItem {
   type: string;
@@ -58,8 +63,12 @@ const MediaReviews = () => {
   const filterType = filterTabIndex(selectedTab);
   const [showImages, setShowImages] = useState(true);
   const [sortAtoZ, setSortAtoZ] = useState<null | boolean>(true);
-  const [sortRatingDescending, setSortRatingDescending] = useState<null | boolean>(null);
+  const [sortRatingDescending, setSortRatingDescending] = useState<
+    null | boolean
+  >(null);
   const [preferencesLoaded, setPreferencesLoaded] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedSearch, setSelectedSearch] = useState("");
 
   useEffect(() => {
     // Retrieving from local storage if the preference was set
@@ -87,103 +96,114 @@ const MediaReviews = () => {
     );
   });
 
-  const reviews = Reviews.filter((item) => item.type == filterType)
-    .sort((r1, r2) => {
-      if (sortRatingDescending !== null) {
-        if (r1.rating === undefined && r2.rating === undefined) {
-          return 0;
-        }
-        if (r1.rating === undefined) {
-          return 1;
-        } else if (r2.rating === undefined) {
-          return -1;
-        }
-        if (sortRatingDescending) {
-          return r2.rating - r1.rating;
-        } else {
-          return r1.rating - r2.rating;
-        }
+  const reviews = Reviews.filter(
+    (item) =>
+      item.type == filterType &&
+      (selectedSearch === "" ? true : item.name === selectedSearch)
+  ).sort((r1, r2) => {
+    if (sortRatingDescending !== null) {
+      if (r1.rating === undefined && r2.rating === undefined) {
+        return 0;
       }
-      if (sortAtoZ) {
-        return r1.name.localeCompare(r2.name);
+      if (r1.rating === undefined) {
+        return 1;
+      } else if (r2.rating === undefined) {
+        return -1;
+      }
+      if (sortRatingDescending) {
+        return r2.rating - r1.rating;
       } else {
-        return -r1.name.localeCompare(r2.name);
+        return r1.rating - r2.rating;
       }
-    })
-    .map((item: ReviewItem, index) => {
-      const reviewParagraphs = item.review?.map((para, index) => {
-        return <p key={index}>{para}</p>;
-      });
-      const subReviews = item.reviewSubItems?.map((subItem, index) => {
-        const subReviewParagraphs = subItem.review?.map((c, index) => {
-          return <p key={index}>{c}</p>;
-        });
-        return (
-          <div
-            key={index}
-            className="bg-slate-100 dark:bg-zinc-900 p-4 md:p-8 rounded-lg"
-          >
-            <div className="flex justify-between items-center mb-4">
-              <div className="font-bold text-xl w-4/5">{subItem.name}</div>
-              {subItem.rating && (
-                <div
-                  className={`rounded-full w-8 h-8 flex ${ratingColour(
-                    subItem.rating
-                  )}`}
-                >
-                  <p className="m-auto font-bold">{subItem.rating}</p>
-                </div>
-              )}
-            </div>
-            {subItem.image !== undefined && showImages && (
-              <div className={"w-full h-36 md:h-72 relative my-4 "}>
-                <Image
-                  alt="Cover Image"
-                  src={subItem.image}
-                  fill={true}
-                  style={{ objectFit: "contain" }}
-                />
-              </div>
-            )}
-            <div className="flex flex-col gap-2">{subReviewParagraphs}</div>
-          </div>
-        );
-      });
+    }
+    if (sortAtoZ) {
+      return r1.name.localeCompare(r2.name);
+    } else {
+      return -r1.name.localeCompare(r2.name);
+    }
+  });
 
+  const reviewElements = reviews.map((item: ReviewItem, index) => {
+    const reviewParagraphs = item.review?.map((para, index) => {
+      return <p key={index}>{para}</p>;
+    });
+    const subReviews = item.reviewSubItems?.map((subItem, index) => {
+      const subReviewParagraphs = subItem.review?.map((c, index) => {
+        return <p key={index}>{c}</p>;
+      });
       return (
         <div
           key={index}
-          className="bg-slate-50 dark:bg-slate-800 p-4 md:p-8 w-full md:w-5/6 lg:w-2/3 2xl:w-1/2 rounded-lg animate-fade"
+          className="bg-slate-100 dark:bg-zinc-900 p-4 md:p-8 rounded-lg"
         >
-          <div className="flex justify-between items-center mb-2">
-            <div className="font-bold text-2xl w-4/5 ml-2">{item.name}</div>
-            {item.rating && (
+          <div className="flex justify-between items-center mb-4">
+            <div className="font-bold text-xl w-4/5">{subItem.name}</div>
+            {subItem.rating && (
               <div
-                className={`rounded-full text-2xl w-14 h-14 flex ${ratingColour(
-                  item.rating
+                className={`rounded-full w-8 h-8 flex ${ratingColour(
+                  subItem.rating
                 )}`}
               >
-                <p className="m-auto font-bold">{item.rating}</p>
+                <p className="m-auto font-bold">{subItem.rating}</p>
               </div>
             )}
           </div>
-          {item.image !== undefined && showImages && (
-            <div className="w-full h-36 md:h-72 relative my-4">
+          {subItem.image !== undefined && showImages && (
+            <div className={"w-full h-36 md:h-72 relative my-4 "}>
               <Image
                 alt="Cover Image"
-                src={item.image}
+                src={subItem.image}
                 fill={true}
                 style={{ objectFit: "contain" }}
               />
             </div>
           )}
-          <div className="flex flex-col gap-2">{reviewParagraphs}</div>
-          <div className="flex flex-col py-4 md:p-4 gap-4 md:gap-8">
-            {subReviews}
-          </div>
+          <div className="flex flex-col gap-2">{subReviewParagraphs}</div>
         </div>
       );
     });
+
+    return (
+      <div
+        key={index}
+        className="bg-slate-50 dark:bg-slate-800 p-4 md:p-8 w-full md:w-5/6 lg:w-2/3 2xl:w-1/2 rounded-lg animate-fade"
+      >
+        <div className="flex justify-between items-center mb-2">
+          <div className="font-bold text-2xl w-4/5 ml-2">{item.name}</div>
+          {item.rating && (
+            <div
+              className={`rounded-full text-2xl w-14 h-14 flex ${ratingColour(
+                item.rating
+              )}`}
+            >
+              <p className="m-auto font-bold">{item.rating}</p>
+            </div>
+          )}
+        </div>
+        {item.image !== undefined && showImages && (
+          <div className="w-full h-36 md:h-72 relative my-4">
+            <Image
+              alt="Cover Image"
+              src={item.image}
+              fill={true}
+              style={{ objectFit: "contain" }}
+            />
+          </div>
+        )}
+        <div className="flex flex-col gap-2">{reviewParagraphs}</div>
+        <div className="flex flex-col py-4 md:p-4 gap-4 md:gap-8">
+          {subReviews}
+        </div>
+      </div>
+    );
+  });
+
+  const filteredReviewSearches =
+    searchQuery === ""
+      ? reviews
+      : reviews.filter((review) => {
+          return review.name.toLowerCase().includes(searchQuery.toLowerCase());
+        });
 
   return (
     <div>
@@ -241,8 +261,38 @@ const MediaReviews = () => {
       <div className="flex gap-2 md:gap-6 justify-center my-4 flex-wrap">
         {tabs}
       </div>
+      <div className="w-72 md:w-96 mx-auto my-4">
+        <Combobox value={selectedSearch} onChange={setSelectedSearch}>
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white dark:bg-black text-left focus:outline-none">
+            <Combobox.Input
+              onChange={(event) => {
+                setSearchQuery(event.target.value);
+                if (event.target.value === "") {
+                  setSelectedSearch("");
+                }
+              }}
+              displayValue={(review: string) => review}
+              className="dark:bg-zinc-900 w-full pl-3 py-2 pr-10 rounded-lg focus:outline-none"
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+              <AiOutlineDown className="h-5 w-5" aria-hidden="true" />
+            </Combobox.Button>
+          </div>
+          <Combobox.Options className="absolute bg-white dark:bg-black w-72 md:w-96 rounded-lg max-h-60 md:max-h-80 overflow-auto mt-1">
+            {filteredReviewSearches.map((review) => (
+              <Combobox.Option
+                key={review.name}
+                value={review.name}
+                className="px-4 cursor-pointer py-2"
+              >
+                {review.name}
+              </Combobox.Option>
+            ))}
+          </Combobox.Options>
+        </Combobox>
+      </div>
       <div className="flex flex-col gap-8 items-center px-4 mb-12">
-        {reviews}
+        {reviewElements}
       </div>
     </div>
   );
