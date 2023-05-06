aa92460e401db233a0f2025e7bafbf8d579267d1
Ashwin Gur <ashwingur88@gmail.com>
2023-05-06 14:37:48 +1000

    Fixed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import { Combobox } from "@headlessui/react";
 
 const Footer = () => {
   return (
-    <div className="bg-slate-900 text-white flex py-2 ml-4 md:ml-12 gap-4">
+    <div className="bg-slate-900 text-white flex py-2 pl-4 md:pl-12 gap-4">
       <div>Ashwin Gur</div>
       <Link href={"mailto:agur9842@uni.sydney.edu.au"}>
         E: agur9842@uni.sydney.edu.au
