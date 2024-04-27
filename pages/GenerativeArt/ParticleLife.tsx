74030482b0a94ff9d2f8732110f17df1a3297eb1
Ashwin Gur <ashwingur88@gmail.com>
2024-04-27 18:03:16 +1000

    made a basic emergence sim work

--- pages/GenerativeArt/ParticleLife.tsx
+++ pages/GenerativeArt/ParticleLife.tsx
@@ -7,7 +7,7 @@ const ParticleLife = () => {
   const width = 1200;
   const height = 800;
 
-  const sketch: Sketch = (p: P5CanvasInstance) => {
+  const sketch: Sketch = (p5: P5CanvasInstance) => {
     enum Colour {
       Red = 0,
       Orange = 1,
@@ -20,20 +20,21 @@ const ParticleLife = () => {
     let particleRuleSet: ParticleRuleSet;
     const n = 100;
     const nColours = 6;
+    const dampingFactor = 0.99;
     class Particle {
       position: p5.Vector;
       velocity: p5.Vector;
       acceleration: p5.Vector;
       p5Colour: p5.Color;
       constructor(x: number, y: number, public colour: Colour) {
-        this.position = p.createVector(x, y);
-        this.velocity = p.createVector(0, 0);
-        this.acceleration = p.createVector(0, 0);
-        this.p5Colour = p.color(0, 0, 0);
+        this.position = p5.createVector(x, y);
+        this.velocity = p5.createVector(0, 0);
+        this.acceleration = p5.createVector(0, 0);
+        this.p5Colour = p5.color(0, 0, 0);
         this.setColour(colour);
       }
       update() {
-        this.velocity.add(this.acceleration);
+        this.velocity.add(this.acceleration).mult(dampingFactor);
         this.position.add(this.velocity);
         // Check if its gone off the screen, wrap it back around
         if (this.position.x < 0) {
@@ -56,25 +57,25 @@ const ParticleLife = () => {
       setColour(colour: Colour) {
         switch (colour) {
           case Colour.Red:
-            this.p5Colour = p.color(235, 64, 52);
+            this.p5Colour = p5.color(235, 64, 52);
             break;
           case Colour.Orange:
-            this.p5Colour = p.color(235, 147, 52);
+            this.p5Colour = p5.color(235, 147, 52);
             break;
           case Colour.Yellow:
-            this.p5Colour = p.color(255, 242, 99);
+            this.p5Colour = p5.color(255, 242, 99);
             break;
           case Colour.Green:
-            this.p5Colour = p.color(64, 235, 52);
+            this.p5Colour = p5.color(64, 235, 52);
             break;
           case Colour.Blue:
-            this.p5Colour = p.color(52, 134, 235);
+            this.p5Colour = p5.color(52, 134, 235);
             break;
           case Colour.Purple:
-            this.p5Colour = p.color(168, 52, 235);
+            this.p5Colour = p5.color(168, 52, 235);
             break;
           default:
-            this.p5Colour = p.color(0, 0, 0);
+            this.p5Colour = p5.color(0, 0, 0);
         }
       }
     }
@@ -83,7 +84,6 @@ const ParticleLife = () => {
     class ParticleRuleSet {
       ruleMatrix: number[][];
       constructor(
-        private universal_repulsion_force: number,
         private universal_repulsion_distance: number,
         private max_force_distance: number
       ) {
@@ -96,7 +96,7 @@ const ParticleLife = () => {
             if (i === j) {
               this.ruleMatrix[i][j] = 1;
             } else {
-              this.ruleMatrix[i][j] = p.random(-1, 2);
+              this.ruleMatrix[i][j] = p5.random(-1, 2);
             }
           }
         }
@@ -104,26 +104,36 @@ const ParticleLife = () => {
       // Calculate the force to be applied to particle a based on the characteristics of
       // particle b
       forceOnParticle(a: Particle, b: Particle): p5.Vector {
-        const distance = a.position.dist(b.position);
-        if (distance >= this.max_force_distance) {
+        const r = a.position.dist(b.position);
+        if (
+          r >=
+          (this.max_force_distance - this.universal_repulsion_distance) * 2 +
+            this.universal_repulsion_distance
+        ) {
           // If the particles are too far, then no force should be applied
-          return p.createVector(0, 0);
-        } else if (distance <= this.universal_repulsion_distance) {
+          return p5.createVector(0, 0);
+        } else if (r <= this.universal_repulsion_distance) {
           // The particles are too close to each other, apply the universal repulsion force
           // to prevent them from converging to a single point
 
           // COULDNT USE p5.Vector static method for some reason, cause window not defined error???
           // So I'm creating and doing the subtraction myself
-          p.createVector(
-            b.position.x - a.position.x,
-            b.position.y - a.position.y
-          )
+          return p5
+            .createVector(
+              b.position.x - a.position.x,
+              b.position.y - a.position.y
+            )
             .normalize()
-            .mult(this.universal_repulsion_force);
+            .mult(r / this.universal_repulsion_distance - 3);
         } else {
           // Look at the rule set for the correct force to apply
-          const force = this.ruleMatrix[a.colour][b.colour];
-          return p
+          // We use an absolute value cartesian equation
+          const F = this.ruleMatrix[a.colour][b.colour];
+          const force =
+            (F /
+              (this.max_force_distance - this.universal_repulsion_distance)) *
+            Math.abs(r - this.max_force_distance);
+          return p5
             .createVector(
               b.position.x - a.position.x,
               b.position.y - a.position.y
@@ -131,26 +141,27 @@ const ParticleLife = () => {
             .normalize()
             .mult(force);
         }
-        return p.createVector(0, 0);
+        return p5.createVector(0, 0);
       }
     }
-    p.setup = () => {
-      p.createCanvas(width, height);
+    p5.setup = () => {
+      p5.createCanvas(width, height);
       particles = new Array();
       for (let i = 0; i < n; i++) {
         particles.push(
           new Particle(
-            p.random(0, width - 1),
-            p.random(0, height - 1),
+            p5.random(0, width - 1),
+            p5.random(0, height - 1),
             Colour.Green
           )
         );
       }
-      particleRuleSet = new ParticleRuleSet(-2, 10, 50);
+      particleRuleSet = new ParticleRuleSet(200, 1000);
+      p5.noStroke();
       //   p5.frameRate(1);
     };
-    p.draw = () => {
-      p.background(0);
+    p5.draw = () => {
+      p5.background(0);
       for (let i = 0; i < n; i++) {
         // Loop through every other particle and apply the force to it
         for (let j = 0; j < n; j++) {
@@ -164,8 +175,8 @@ const ParticleLife = () => {
           particles[i].applyForce(forceToApply);
         }
         particles[i].update();
-        p.fill(particles[i].p5Colour);
-        p.circle(particles[i].position.x, particles[i].position.y, 10);
+        p5.fill(particles[i].p5Colour);
+        p5.circle(particles[i].position.x, particles[i].position.y, 10);
       }
     };
   };
