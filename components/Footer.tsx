29b52b6266aaf20f098eae19367c40a046f83230
Ashwin Gur <ashwingur88@gmail.com>
2024-05-08 17:31:27 +1000

    Updated email in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,9 +6,7 @@ const Footer = () => {
   return (
     <div className="bg-slate-900 text-white flex py-2 pl-4 md:pl-12 gap-4">
       <div>Ashwin Gur</div>
-      <Link href={"mailto:agur9842@uni.sydney.edu.au"}>
-        E: agur9842@uni.sydney.edu.au
-      </Link>
+      <Link href={"mailto:ashwingur1@gmail.com"}>E: ashwingur1@gmail.com</Link>
     </div>
   );
 };
