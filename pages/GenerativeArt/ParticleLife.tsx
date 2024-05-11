922184eff3e6c7b97a95ebd8c61acda68de6e09b
Ashwin Gur <ashwingur88@gmail.com>
2024-05-11 20:36:13 +1000

    Added background and padding to matrix visual

--- pages/GenerativeArt/ParticleLife.tsx
+++ pages/GenerativeArt/ParticleLife.tsx
@@ -309,7 +309,7 @@ const ParticleLife = () => {
             Zero Matrix
           </button>
         </div>
-        <div className="flex flex-col mb-4">
+        <div className="flex flex-col mb-4 bg-gray-500 p-2 rounded-lg">
           <div className="flex justify-end gap-1 mb-1">{topColourRow}</div>{" "}
           {matrixButtons}
         </div>
