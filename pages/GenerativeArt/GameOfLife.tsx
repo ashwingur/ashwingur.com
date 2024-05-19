c89e638d39c2d1ff14442a86239cf0b906677c2d
Ashwin Gur <ashwingur88@gmail.com>
2024-05-19 19:32:02 +1000

    Changed game of life resolution

--- pages/GenerativeArt/GameOfLife.tsx
+++ pages/GenerativeArt/GameOfLife.tsx
@@ -12,7 +12,7 @@ const GameOfLife = () => {
     let textCanvas: p5.Graphics;
 
     p5.setup = () => {
-      width = Math.min(p5.windowWidth - 20, 1200);
+      width = Math.min(p5.windowWidth - 20, 800);
       height = Math.min(p5.windowWidth - 20, 800);
       p5.createCanvas(width, height, p5.WEBGL);
       myShader = p5.createShader(vert, frag);
