941289de84bc86295f771a9ae169377540a46487
Ashwin Gur <ashwingur88@gmail.com>
2024-06-30 00:19:25 +1000

    styling improvements

--- components/mediareviews/ListEditableReviews.tsx
+++ components/mediareviews/ListEditableReviews.tsx
@@ -26,10 +26,24 @@ const ListEditableReviews: React.FC<ListEditableReviewsProps> = ({
       deleteReviewMutation.isLoading &&
       deleteReviewMutation.variables === review.id;
     return (
-      <Card key={review.id} firstLayer={true} className="flex flex-col">
-        <p>ID: {review.id}</p>
-        <p>Name: {review.name}</p>
-        <p>Type: {review.media_type}</p>
+      <Card key={review.id} firstLayer={true} className="flex flex-col mx-auto">
+        <h3 className="text-center">{review.name}</h3>
+        <p>
+          <span className="font-bold">ID:</span> {review.id}
+        </p>
+        <p>
+          <span className="font-bold">Type:</span> {review.media_type}
+        </p>
+        <p>
+          <span className="font-bold">Created:</span>{" "}
+          {review.review_creation_date &&
+            `${new Date(review.review_creation_date).toLocaleDateString()} ${new Date(review.review_creation_date).toLocaleTimeString()}`}
+        </p>
+        <p>
+          <span className="font-bold">Updated:</span>{" "}
+          {review.review_last_update_date &&
+            `${new Date(review.review_last_update_date).toLocaleDateString()} ${new Date(review.review_last_update_date).toLocaleTimeString()}`}
+        </p>
         <button
           className="btn mx-auto mt-2 w-24 h-10"
           onClick={() => {
@@ -52,7 +66,7 @@ const ListEditableReviews: React.FC<ListEditableReviewsProps> = ({
   });
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:w-4/5 w-full">
       {reviewItems}
     </div>
   );
