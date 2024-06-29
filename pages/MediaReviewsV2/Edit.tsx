b9f85e26145449d426b4d1da95e588679e2a0732
Ashwin Gur <ashwingur88@gmail.com>
2024-06-30 00:40:51 +1000

    Added unauthenticated message

--- pages/MediaReviewsV2/Edit.tsx
+++ pages/MediaReviewsV2/Edit.tsx
@@ -3,6 +3,7 @@ import ConfirmButton from "@components/ConfirmButton";
 import CreateOrUpdateReviewForm from "@components/mediareviews/CreateOrUpdateReviewForm";
 import ListEditableReviews from "@components/mediareviews/ListEditableReviews";
 import Navbar from "@components/navbars/Navbar";
+import { useAuth } from "@context/AuthContext";
 import React, { useState } from "react";
 import { useMediaReviews } from "shared/queries/mediareviews";
 import {
@@ -13,6 +14,7 @@ import {
 const Edit = () => {
   const { data } = useMediaReviews();
   const [currentlyEditing, setCurrentlyEditing] = useState<MediaReview>();
+  const { user, loading } = useAuth();
 
   const handleCreateNew = () => {
     setCurrentlyEditing(getDefaultMediaReview());
@@ -36,6 +38,11 @@ const Edit = () => {
       <Navbar fixed={true} />
       <div className="flex flex-col items-center">
         <h1 className="mb-4">Edit Reviews</h1>
+        {user === null && !loading && (
+          <p className="bg-red-600 rounded-lg text-white mb-4 p-2">
+            You are currently unauthenticated
+          </p>
+        )}
         {!currentlyEditing && (
           <button className="btn mb-4" onClick={handleCreateNew}>
             Create New
