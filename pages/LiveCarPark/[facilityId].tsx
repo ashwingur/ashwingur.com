1cd6c8e5d6c4e206aab070905c81808d0d9bd342
Ashwin Gur <ashwingur88@gmail.com>
2025-03-20 20:18:58 +1100

    Parking page improvements

--- pages/LiveCarPark/[facilityId].tsx
+++ pages/LiveCarPark/[facilityId].tsx
@@ -3,7 +3,10 @@ import DateTimeRangePicker from "@components/DateTimeRangePicker";
 import GenericListbox from "@components/GenericListBox";
 import LoadingIcon from "@components/LoadingIcon";
 import Navbar from "@components/navbars/Navbar";
+import { isDark } from "@components/ToggleThemeButton";
 import TimeSeriesChart from "@components/weather/TimeSeriesChart";
+import clsx from "clsx";
+import { useTheme } from "next-themes";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
@@ -12,6 +15,7 @@ import { createTimeOptions, TimeOption } from "shared/timeoptions";
 
 const FacilityHistory = () => {
   const router = useRouter();
+  const { theme } = useTheme();
 
   const facilityId =
     typeof router.query?.facilityId === "string"
@@ -71,6 +75,15 @@ const FacilityHistory = () => {
     );
   }
 
+  const occupied = Math.min(data.latest_occupancy, data.capacity);
+  const percentOccupied = occupied / data.capacity;
+  let bgClassName = isDark(theme) ? "bg-green-600" : "bg-green-500";
+  if (percentOccupied >= 0.85) {
+    bgClassName = isDark(theme) ? "bg-red-700" : "bg-red-600";
+  } else if (percentOccupied >= 0.5) {
+    bgClassName = isDark(theme) ? "bg-amber-500" : "bg-amber-500";
+  }
+
   return (
     <div className="min-h-screen">
       <Navbar fixed={true} />
@@ -82,6 +95,32 @@ const FacilityHistory = () => {
           Back
         </Link>
       </div>
+      <Card firstLayer={true} className="mx-4 mb-6 max-w-96 md:mx-auto">
+        <div className="group flex justify-between">
+          <div className="flex">
+            <div
+              className={clsx(
+                "mr-2 h-full w-4 rounded-md md:mr-4",
+                bgClassName,
+              )}
+            />
+            <div className="flex flex-col">
+              <div className="flex items-center">
+                <p className="font-bold">
+                  {data.facility_name.replace(/^Park&Ride - /, "")}
+                </p>
+              </div>
+              <p>
+                Occupied: {occupied}/{data.capacity}
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="text-2xl">{data.capacity - occupied}</p>
+            <p className="text-sm">Available</p>
+          </div>
+        </div>
+      </Card>
       <Card firstLayer={true} className="mx-4 md:mx-auto md:w-4/5">
         <div className="z-20 flex flex-col items-center">
           <h3 className="mb-2 text-xl">Time Filter</h3>
@@ -110,7 +149,8 @@ const FacilityHistory = () => {
         </div>
         <div className="mt-2 flex items-center justify-center gap-4 md:gap-8">
           <p>
-            <span className="font-bold">Current:</span> {data.latest_occupancy}
+            <span className="font-bold">Current:</span> {data.latest_occupancy}{" "}
+            ({Math.round((100 * data.latest_occupancy) / data.capacity)}%)
           </p>
           <p>
             <span className="font-bold">Min: </span>
