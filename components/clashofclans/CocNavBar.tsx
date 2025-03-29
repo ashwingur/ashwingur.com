19a5ec50a16ef5345b6faed5248e418d4c9bcd6e
Ashwin Gur <ashwingur88@gmail.com>
2025-03-29 17:11:15 +1100

    Improved clash of clans navbar

--- components/clashofclans/CocNavBar.tsx
+++ components/clashofclans/CocNavBar.tsx
@@ -3,9 +3,17 @@ import React, { useState } from "react";
 import { ToggleThemeButton } from "../ToggleThemeButton";
 import { BiHash, BiSearchAlt2 } from "react-icons/bi";
 import { useRouter } from "next/router";
-import { MdOutlineMenu } from "react-icons/md";
+import {
+  MdLogin,
+  MdOutlineClose,
+  MdOutlineMenu,
+  MdSettings,
+} from "react-icons/md";
 import CocButton from "./CocButton";
 import CustomisableLogo from "@components/CustomisableLogo";
+import { useTheme } from "next-themes";
+import { useAuth } from "@context/AuthContext";
+import clsx from "clsx";
 
 interface Tags {
   playerTag: string;
@@ -15,6 +23,9 @@ interface Tags {
 const CocNavBar = () => {
   const router = useRouter();
   const [showMobileNav, setShowMobileNav] = useState(false);
+  const { theme } = useTheme();
+  const [mobileNavMenu, setMobileNavMenu] = useState(false); // Mobile nav menu not showing at the start
+  const { user, role } = useAuth();
 
   // The current value of the input fields
   const [inputFieldTags, setInputFieldTags] = useState<Tags>({
@@ -52,91 +63,29 @@ const CocNavBar = () => {
   };
 
   return (
-    <div className="text-slate-900">
-      <div className="fixed z-50 flex w-full justify-between bg-white/30 px-4 py-4 shadow-lg backdrop-blur-md dark:bg-black/30 md:px-8 lg:px-16">
-        <Link href="/">
-          <CustomisableLogo className="bg-[#6c779b] text-white" />
-        </Link>
-        {/* Input Fields */}
-        <div className="hidden flex-col items-center justify-center gap-8 font-coc font-thin md:flex md:flex-row">
-          <div className="flex items-center">
-            <input
-              className="w-40 rounded-sm border-2 px-2 py-1 uppercase"
-              placeholder="PLAYER TAG"
-              value={inputFieldTags.playerTag}
-              onChange={updateTagInput}
-              name="playerTag"
-            />
-            <button
-              onClick={() => {
-                setPlayerTag(inputFieldTags.playerTag);
-              }}
-              className="ml-2 rounded-lg bg-black p-1 text-yellow-500 transition-all hover:text-red-600"
-            >
-              <BiSearchAlt2 size={30} />
-            </button>
-          </div>
+    <div
+      className={clsx(
+        "fixed top-0 z-50 flex flex-col",
 
-          <div className="flex items-center">
-            <input
-              className="w-40 rounded-sm border-2 px-2 py-1 uppercase"
-              placeholder="CLAN TAG"
-              value={inputFieldTags.clanTag}
-              onChange={updateTagInput}
-              name="clanTag"
-            />
-            <button
-              onClick={() => {
-                setClanTag(inputFieldTags.clanTag);
-              }}
-              className="ml-2 rounded-lg bg-black p-1 text-yellow-500 transition-all hover:text-red-600"
-            >
-              <BiSearchAlt2 size={30} />
-            </button>
-          </div>
-        </div>
-        <div className="hidden md:block">
-          <Link
-            href="/ClashOfClans"
-            className="mr-4 rounded-md px-2 py-1 transition hover:bg-blue-100 dark:hover:bg-black"
-            scroll={false}
-          >
-            Clash Home
-          </Link>
-          <ToggleThemeButton />
-        </div>
-        <div
-          className="cursor-pointer md:hidden"
-          onClick={() => {
-            setShowMobileNav(true);
-          }}
-        >
-          <MdOutlineMenu size={25} />
-        </div>
-      </div>
-      {/* MOBILE NAV MENU */}
+        mobileNavMenu ? "h-screen" : "",
+      )}
+    >
       <div
-        className={
-          showMobileNav
-            ? "fixed left-0 top-0 z-50 h-screen w-full bg-black/70 transition-all"
-            : "z-50 bg-black"
-        }
-        onClick={() => setShowMobileNav(false)}
+        className={clsx(
+          "flex w-screen flex-col border-b border-background-muted bg-white/30 font-clash font-thin shadow-lg backdrop-blur-lg",
+        )}
       >
         <div
-          className={
-            showMobileNav
-              ? "absolute left-0 top-0 z-50 h-full w-[75%] bg-gradient-to-b from-[#8c94ac] to-[#6c779b] duration-500 ease-in"
-              : "fixed left-[-100%] top-0 z-50 h-full bg-gradient-to-b from-[#8c94ac] to-[#6c779b] p-10 duration-500 ease-in"
-          }
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
+          className={clsx(
+            "flex w-full items-center justify-between px-6 py-2 md:px-8 lg:px-16",
+          )}
         >
+          <Link href="/">
+            <CustomisableLogo className="bg-[#6c779b] text-white" />
+          </Link>
           {/* Input Fields */}
-          <div className="flex flex-col items-center justify-center gap-8 font-coc font-thin md:flex-row">
-            <div className="mt-8 flex items-center">
-              <BiHash size={25} />
+          <div className="hidden flex-col items-center justify-center gap-8 font-coc font-thin md:flex md:flex-row">
+            <div className="flex items-center">
               <input
                 className="w-40 rounded-sm border-2 px-2 py-1 uppercase"
                 placeholder="PLAYER TAG"
@@ -144,19 +93,19 @@ const CocNavBar = () => {
                 onChange={updateTagInput}
                 name="playerTag"
               />
-              <button
-                onClick={() => {
-                  setPlayerTag(inputFieldTags.playerTag);
-                  setShowMobileNav(false);
-                }}
-                className="ml-2 rounded-lg bg-black p-1 text-yellow-500 transition-all hover:text-red-600"
+              <Link
+                className={clsx(
+                  "ml-2 rounded-lg bg-black p-1 text-yellow-500 transition-all hover:text-red-600",
+                  inputFieldTags.playerTag.replace("#", "") === "" &&
+                    "pointer-events-none",
+                )}
+                href={`/ClashOfClans/player/${inputFieldTags.playerTag.replace("#", "")}`}
               >
                 <BiSearchAlt2 size={30} />
-              </button>
+              </Link>
             </div>
 
             <div className="flex items-center">
-              <BiHash size={25} />
               <input
                 className="w-40 rounded-sm border-2 px-2 py-1 uppercase"
                 placeholder="CLAN TAG"
@@ -164,36 +113,173 @@ const CocNavBar = () => {
                 onChange={updateTagInput}
                 name="clanTag"
               />
-              <button
-                onClick={() => {
-                  setClanTag(inputFieldTags.clanTag);
-                  setShowMobileNav(false);
-                }}
-                className="ml-2 rounded-lg bg-black p-1 text-yellow-500 transition-all hover:text-red-600"
+              <Link
+                className={clsx(
+                  "ml-2 rounded-lg bg-black p-1 text-yellow-500 transition-all hover:text-red-600",
+                  inputFieldTags.clanTag.replace("#", "") === "" &&
+                    "pointer-events-none",
+                )}
+                href={`/ClashOfClans/clan/${inputFieldTags.clanTag.replace("#", "")}`}
               >
                 <BiSearchAlt2 size={30} />
-              </button>
-            </div>
-            <div className="flex h-16 items-center">
-              <Link href={"/ClashOfClans"}>
-                <CocButton
-                  className="w-40 hover:w-32"
-                  text={"Home"}
-                  innerColour="bg-blue-500"
-                  middleColour="bg-blue-600"
-                  outerColour="bg-blue-700"
-                  onClick={() => {
-                    setShowMobileNav(false);
-                  }}
-                />
               </Link>
             </div>
-            <div className="">
+          </div>
+          <ul className="hidden lg:flex lg:items-center lg:gap-4">
+            <Link
+              href="/ClashOfClans"
+              className="rounded-md px-2 py-1 transition hover:bg-background-hover"
+              scroll={false}
+            >
+              <li>Clash Home</li>
+            </Link>
+
+            {user && role === "admin" && (
+              <Link
+                href="/Admin"
+                className="rounded-md px-2 py-1 transition hover:bg-background-hover"
+                scroll={false}
+              >
+                <li>Admin</li>
+              </Link>
+            )}
+            <li>
               <ToggleThemeButton />
-            </div>
+            </li>
+
+            {!user && (
+              <Link
+                href="/Login"
+                className="rounded-md px-2 py-2 transition hover:bg-background-hover"
+              >
+                <MdLogin />
+              </Link>
+            )}
+            {user && (
+              <Link
+                href="/Login"
+                className="rounded-md px-2 py-2 transition hover:bg-background-hover"
+              >
+                <MdSettings />
+              </Link>
+            )}
+          </ul>
+          <div
+            className="cursor-pointer lg:hidden"
+            onClick={() => {
+              setMobileNavMenu(!mobileNavMenu);
+            }}
+          >
+            {mobileNavMenu ? (
+              <MdOutlineClose size={30} />
+            ) : (
+              <MdOutlineMenu size={30} />
+            )}
           </div>
         </div>
+        {/* MOBILE NAV MENU */}
+        <div
+          className={clsx(
+            "relative z-50 mx-4 flex justify-between duration-300 md:mx-6 lg:hidden",
+            mobileNavMenu
+              ? "box-border min-h-full border-t border-background-muted"
+              : "hidden",
+          )}
+        >
+          <ul className="w-30 my-4 flex flex-col gap-2">
+            <Link
+              href="/ClashOfClans"
+              className="rounded-md px-2 py-2 hover:bg-background-hover"
+              scroll={false}
+              onClick={() => {
+                setMobileNavMenu(false);
+              }}
+            >
+              <li>Clash Home</li>
+            </Link>
+
+            {user && role === "admin" && (
+              <Link
+                href="/Admin"
+                className="rounded-md px-2 py-2 hover:bg-background-hover"
+                scroll={false}
+                onClick={() => {
+                  setMobileNavMenu(false);
+                }}
+              >
+                <li>Admin</li>
+              </Link>
+            )}
+
+            {/* Input Fields */}
+            <div className="flex items-center md:hidden">
+              <input
+                className="w-40 rounded-sm border-2 px-2 py-1 uppercase"
+                placeholder="PLAYER TAG"
+                value={inputFieldTags.playerTag}
+                onChange={updateTagInput}
+                name="playerTag"
+              />
+              <Link
+                className={clsx(
+                  "ml-2 rounded-lg bg-black p-1 text-yellow-500 transition-all hover:text-red-600",
+                  inputFieldTags.playerTag.replace("#", "") === "" &&
+                    "pointer-events-none",
+                )}
+                href={`/ClashOfClans/player/${inputFieldTags.playerTag.replace("#", "")}`}
+              >
+                <BiSearchAlt2 size={30} />
+              </Link>
+            </div>
+
+            <div className="flex items-center md:hidden">
+              <input
+                className="w-40 rounded-sm border-2 px-2 py-1 uppercase"
+                placeholder="CLAN TAG"
+                value={inputFieldTags.clanTag}
+                onChange={updateTagInput}
+                name="clanTag"
+              />
+              <Link
+                className={clsx(
+                  "ml-2 rounded-lg bg-black p-1 text-yellow-500 transition-all hover:text-red-600",
+                  inputFieldTags.clanTag.replace("#", "") === "" &&
+                    "pointer-events-none",
+                )}
+                href={`/ClashOfClans/clan/${inputFieldTags.clanTag.replace("#", "")}`}
+              >
+                <BiSearchAlt2 size={30} />
+              </Link>
+            </div>
+          </ul>
+          <ul className="my-4 flex flex-col items-center gap-2">
+            {!user && (
+              <Link
+                href="/Login"
+                className="rounded-md px-4 py-4 hover:bg-background-hover"
+              >
+                <MdLogin />
+              </Link>
+            )}
+            {user && (
+              <Link
+                href="/Login"
+                className="my-2 rounded-md px-4 py-4 hover:bg-background-hover"
+              >
+                <MdSettings />
+              </Link>
+            )}
+
+            <ToggleThemeButton />
+          </ul>
+        </div>
       </div>
+      {mobileNavMenu && (
+        <div
+          className="h-full bg-black/50"
+          onClick={() => setMobileNavMenu(false)}
+        ></div>
+      )}
     </div>
   );
 };
